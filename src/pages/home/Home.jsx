46b178d927095a57895af211d059026c9c11ad44
BahromNigmanbekov <bahromnigmanbekov@gmail.com>
2025-12-08 18:06:20 +0500

    narsala ochirldi

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Home() {
+  return (
+    <>
+      <div className="big">
+        <div className="container">
+            
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
