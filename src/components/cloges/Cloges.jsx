0e789a76b80c65bd7d4771e27983e947a717b102
xasansharipov1 <sharipovvxasan7@gmail.com>
2025-12-08 18:57:34 +0500

    cloges ozgardi

--- src/components/cloges/Cloges.jsx
+++ src/components/cloges/Cloges.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import page1 from "../../assets/Ellipse 7.png"
+import page2 from "../../assets/Ellipse 8.png"
+import page3 from "../../assets/Ellipse 9.png"
+import page4 from "../../assets/Ellipse 10.png"
+import page5 from "../../assets/Ellipse 11.png"
+import page6 from "../../assets/Ellipse 12.png"
 
 function Cloges() {
   return (
-    <div>
-      <h1>Xasan push qildi</h1>
+    <>
+    <div className="container">
+        <div className="members">
+            <h1 className="about">What <span className="member">Our Member's</span> <br /> Saying About Us</h1>
+            <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Sem velit viverra amet faucibus.</p>
+            <div className="pagess">
+                <img src={page1} alt="" />
+                
+            </div>
+        </div>
     </div>
+    </>
   );
 }
 
