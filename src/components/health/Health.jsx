abe3d917d0847a863358ae9b27f2d07117251c9f
xasansharipov1 <sharipovvxasan7@gmail.com>
2025-12-14 18:06:51 +0500

    cloges togirlandi

--- src/components/health/Health.jsx
+++ src/components/health/Health.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import "./health.css"
+import doctors1 from "../../assets/Rectangle 15.png"
 
 function Health() {
   return (
     <>
-    <h1>salom</h1></>
+      <div className="container">
+        <div className="future">
+
+          <div className="future_wrapper">
+
+            <div className="future_left">
+              <h1 className="title">
+                The Future <br /> of <span>Quality Health</span>
+              </h1>
+
+              <p className="future_p">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec risus 
+                feugiat lectus risus sed ullamcorper. Auctor semper fermentum 
+                volutpat integer vel. In rhoncus elementum nunc, malesuada mi  sed.
+                Nibh est sit lobortis id semper.
+              </p>
+
+              <p className="future_p">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec risus
+                feugiat lectus risus sed ullamcorper. Auctor semper fermentum
+                volutpat integer vel. In rhoncus elementum nunc, malesuada mi sed.
+                Nibh est sit lobortis id semper.
+              </p>
+
+              <p className="future_p">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec risus
+                feugiat lectus risus sed ullamcorper. sed. Nibh est sit lobortis id
+                semper.
+              </p>
+
+              <button className="learn_more">Learn More</button>
+            </div>
+
+            <div className="future_right">
+              <img src={doctors1} alt="" />
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Cloges from "../../components/cloges/Cloges";
 import Header from "../../components/header/Header";
 import Section from "../../components/section/Section";
+import Health from "../../components/health/Health";
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
         <Header />
         <Section />
         <Cloges />
+        <Health/>
       </div>
     </>
   );
